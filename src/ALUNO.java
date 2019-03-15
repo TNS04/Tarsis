d127b16c28a5bf4315d79b992357cbb031820a0a
Tarsis <tarsisn.santos@gmail.com>
2019-03-15 16:50:16 -0300

    primeiro commit

---
+++ src/ALUNO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Társis
+ */
+public class ALUNO {
+   private String nome;
+   private int idade;
+   private char sexo;
+   
+    public void atribuirNome(String nome){
+    
+        this.nome = nome;
+    }
+   public void imprimirNome(){
+   System.out.println(nome);
+   }
+    
+}
